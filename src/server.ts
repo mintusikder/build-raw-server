16b93ede61cf6613a1736bf9947d74984234de9a
Mintu Sikder <mintusikder15@gmail.com>
2026-03-14 13:29:10 +0600

    added build-raw-server

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import { config } from "dotenv";
+import http, { IncomingMessage, Server, ServerResponse } from "http";
+
+const server: Server = http.createServer(
+  (req: IncomingMessage, res: ServerResponse) => {
+    console.log("server is running");
+
+    if (req.url == "/" && req.method === "GET") {
+      res.writeHead(200, {
+        "content-type": "application/json ",
+      });
+      res.end(
+        JSON.stringify({
+          message: "Hello Form Node js",
+          path: req.url,
+        }),
+      );
+    }
+  },
+);
+
+server.listen(5000, () => {
+  console.log(`server is running${5000}`);
+});
