2483372f5ca75621d127b9a898af7cc48a4f2600
Mintu Sikder <mintusikder15@gmail.com>
2026-03-16 16:08:33 +0600

    added node dev

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,11 @@
 import http, { IncomingMessage, Server, ServerResponse } from "http";
 import config from "./config";
+import { json } from "stream/consumers";
 
 const server: Server = http.createServer(
   (req: IncomingMessage, res: ServerResponse) => {
     console.log("server is running ");
-//root route
+    //root route
     if (req.url == "/" && req.method === "GET") {
       res.writeHead(200, {
         "content-type": "application/json ",
@@ -28,7 +29,30 @@ const server: Server = http.createServer(
         }),
       );
     }
-   
+
+    if (req.url == "/api/users" && req.method === "POST") {
+      // const user = {
+      //   id: 1,
+      //   name: "Mintu",
+      // };
+      // res.writeHead(200, {
+      //   "content-type": "application/json ",
+      // });
+      // res.end(JSON.stringify(user));
+      let body = "";
+      req.on("data", (chunk) => {
+        body += chunk.toString();
+      });
+      req.on("end", () => {
+        try {
+          const parsBody = JSON.parse(body);
+          console.log(parsBody);
+          res.end(JSON.stringify(parsBody));
+        } catch (err: any) {
+          console.log(err?.message);
+        }
+      });
+    }
   },
 );
 
